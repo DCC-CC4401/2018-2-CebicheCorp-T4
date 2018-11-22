729746c843e4c0ca88a9478681aa5a21e70590eb
scisneros <sebastian.cisneros384@gmail.com>
2018-11-22 19:02:41 -0300

    Agregar modelos

--- Coevaluaciones/admin.py
+++ Coevaluaciones/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Persona, Coeval, Curso
+from .models import Persona, Coeval, Curso, Rol, Integrante, Pregunta_Coeval, Pregunta, Respuesta
 
 admin.site.register(Persona)
-admin.site.register(Coeval)
 admin.site.register(Curso)
+admin.site.register(Integrante)
+admin.site.register(Rol)
+admin.site.register(Pregunta)
+admin.site.register(Coeval)
+admin.site.register(Pregunta_Coeval)
+admin.site.register(Respuesta)
--- Coevaluaciones/models.py
+++ Coevaluaciones/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.conf import settings
-from django.utils import timezone
+
 
 class Persona(models.Model):
     rut = models.CharField(max_length=13)
     nombre = models.CharField(max_length=100)
+
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=200)
@@ -12,13 +12,41 @@ class Curso(models.Model):
     seccion = models.IntegerField()
     anno = models.IntegerField()
     semestre = models.IntegerField()
-    estudiantes = models.ManyToManyField(Persona, related_name='persona_estudiantes')
-    profesores = models.ManyToManyField(Persona, related_name='persona_profesores')
-    cuerpo_docente = models.ManyToManyField(Persona, related_name='persona_cuerpo_docente', blank=True)
+    integrantes = models.ManyToManyField(Persona, through='Integrante')
+
+
+class Rol(models.Model):
+    rol = models.CharField(max_length=50)
+    icono_archivo = models.CharField(max_length=50)
+
+
+class Integrante(models.Model):
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+
+
+class Pregunta(models.Model):
+    texto = models.CharField(max_length=500)
+
 
 class Coeval(models.Model):
     titulo = models.CharField(max_length=200)
-    fecha_inicio = models.DateTimeField('fecha de inicio')
-    fecha_termino = models.DateTimeField('fecha de término')
+    fecha_inicio = models.DateTimeField()
+    fecha_termino = models.DateTimeField()
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     estado = models.CharField(max_length=20)
+    preguntas = models.ManyToManyField(Pregunta, through='Pregunta_Coeval')
+
+
+class Pregunta_Coeval(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    coeval = models.ForeignKey(Coeval, on_delete=models.CASCADE)
+    orden = models.IntegerField()
+    ponderacion = models.FloatField()
+
+class Respuesta(models.Model):
+    pregunta = models.ForeignKey(Pregunta_Coeval, on_delete=models.CASCADE)
+    nota = models.IntegerField()
+    autor = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='autor')
+    evaluado = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='evaluado')
